8b7fb2c352b4b1451b31fb5b5518d9336f5abdbd
Dincho Todorov <dincho@users.noreply.github.com>
2020-02-01 14:13:09 +0200

    factor out coder

--- coder.js
+++ coder.js
@@ -1,89 +1,62 @@
 const fs = require('fs')
 const blake = require('blakejs')
+const base64check = require('base64check')
 const Serializer = require('./Serializer.js')
-var base64check = require('base64check')
 
 const HASH_BYTES = 32
-
-
-
-const aci = JSON.parse(fs.readFileSync('build/identity.json', 'utf-8'))
-const contractAci = aci[0].contract
 const serializer = Object.create(Serializer)
 
+module.exports = {
+    encode: function (contractAci, funcName, args) {
+        return 'cb_' + base64check.encode(this.serialize(contractAci, funcName, args))
+    },
 
-// console.log(aci)
-// console.log(contractAci)
+    serialize: function (contractAci, funName, args) {
+        const funcAci = contractAci.functions.find(e => e.name == funName)
+        const argAci = (funcAci) ? funcAci.arguments : []
+        // console.log(argAci)
 
-function createCalldata(funName, args) {
-    const funcAci = contractAci.functions.find(e => e.name == funName)
-    const argAci = (funcAci) ? funcAci.arguments : []
-    // console.log(argAci)
+        let serializedArgs = []
+        for (let i = 0; i < argAci.length; i++) {
+            const value = args[i]
+            const type = argAci[i].type
+            const serArg = serializer.serialize(type, value)
+            serializedArgs.push(serArg)
+            // console.log("Serialize arg:", i, value, type, serArg, 
+            //     serArg.map(a => a.toString(2).padStart(8, '0')))
 
-    let serializedArgs = []
-    for (let i = 0; i < argAci.length; i++) {
-        const value = args[i]
-        const type = argAci[i].type
-        const serArg = serializer.serialize(type, value)
-        serializedArgs.push(serArg)
-        // console.log("Serialize arg:", i, value, type, serArg, 
-        //     serArg.map(a => a.toString(2).padStart(8, '0')))
+        }
 
+        // console.log(serializedArgs)
+        // if (serializedArgs.length === 0) {
+        //     const argsTuple = (serializedArgs.length === 0) ? serializeTuple([]) : serializeTuple(serializedArgs)
+        // }
+
+        const functionId = this.symbolIdentifier(funName)
+        // console.log("Function ID:", functionId, serializeByteArray(functionId))
+
+        const argsTuple = serializer.serialize('tuple', serializedArgs)
+        // console.log("args typle:", argsTuple)
+        // console.log(Array.from(functionId).concat(argsTuple))
+
+        const funcTuple = serializer.serialize('tuple', [
+                serializer.serialize('byte_array', functionId),
+                argsTuple
+        ])
+
+        // console.log("function tuple", funcTuple)
+
+        return new Uint8Array(funcTuple.flat(Infinity))
+        // const calldata = serializeTuple(funcTuple)
+
+        // return calldata
+    },
+
+    symbolIdentifier: function (funName) {
+        // First 4 bytes of 32 bytes blake hash
+        hash = Array.from(blake.blake2b(funName, null, HASH_BYTES))
+        // console.log("Blake2b 32 bytes hash", hash)
+
+        return hash.slice(0, 4)
     }
-
-    // console.log(serializedArgs)
-    // if (serializedArgs.length === 0) {
-    //     const argsTuple = (serializedArgs.length === 0) ? serializeTuple([]) : serializeTuple(serializedArgs)
-    // }
-
-    const functionId = symbolIdentifier(funName)
-    // console.log("Function ID:", functionId, serializeByteArray(functionId))
-
-    const argsTuple = serializer.serialize('tuple', serializedArgs)
-    // console.log("args typle:", argsTuple)
-    // console.log(Array.from(functionId).concat(argsTuple))
-
-    const funcTuple = serializer.serialize('tuple', [
-            serializer.serialize('byte_array', functionId),
-            argsTuple
-    ])
-
-    // console.log("function tuple", funcTuple)
-
-    return new Uint8Array(funcTuple.flat(Infinity))
-    // const calldata = serializeTuple(funcTuple)
-
-    // return calldata
 }
-
-function symbolIdentifier(funName) {
-    // First 4 bytes of 32 bytes blake hash
-    hash = Array.from(blake.blake2b(funName, null, 32))
-    // console.log("Blake2b 32 bytes hash", hash)
-
-    return hash.slice(0, 4)
-}
-
-function encodeContractByteArray(byteArray) {
-    return 'cb_' + base64check.encode(byteArray)
-}
-
-console.log(
-    "calldata init()",
-    encodeContractByteArray(createCalldata("init", []))
-)
-
-console.log(
-    "calldata main(true, false)",
-    encodeContractByteArray(createCalldata("main", [true, false]))
-)
-
-console.log(
-    "calldata main(false, true)",
-    encodeContractByteArray(createCalldata("main", [false, true]))
-)
-
-console.log(
-    "calldata main2(42)",
-    encodeContractByteArray(createCalldata("main", [42]))
-)
