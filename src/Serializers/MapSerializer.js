9af83c46a72d42222c505ead83d45ac28e1d91ef
Dincho Todorov <dincho@users.noreply.github.com>
2020-02-17 14:53:48 +0200

    Sort map elements before encoding

--- src/Serializers/MapSerializer.js
+++ src/Serializers/MapSerializer.js
@@ -1,16 +1,21 @@
 const FATE = require('../fate.js')
 const RLPInt = require('../utils/RLPInt.js')
+const FateComparator = require('../FateComparator.js')
 
 MapSerializer = function (globalSerializer) {
     this.globalSerializer = globalSerializer
 }
 
 MapSerializer.prototype = {
-    serialize: function (value) {
+    serialize (value) {
         const [keyKype, valueType, elements] = value
         const len = elements.length
+        const cmp = FateComparator(keyKype)
 
-        const serializedElements = elements.map(e => {
+        const sortedElements = [...elements]
+        sortedElements.sort((elA, elB) => cmp(elA[0], elB[0]))
+
+        const serializedElements = sortedElements.map(e => {
             const [key, value] = e
             return [
                 this.globalSerializer.serialize([keyKype, key]),
@@ -23,7 +28,7 @@ MapSerializer.prototype = {
             ...RLPInt(len),
             ...serializedElements.flat(Infinity)
         ]
-    }
+    },
 }
 
 module.exports = MapSerializer
--- tests/Serializers/MapSerializer.js
+++ tests/Serializers/MapSerializer.js
@@ -4,7 +4,7 @@ const MapSerializer = require('../../src/Serializers/MapSerializer.js')
 
 const s = new MapSerializer(Object.create(Serializer))
 
-test('Serialize', t => {
+test.only('Serialize', t => {
     t.deepEqual(s.serialize(['int', 'bool', []]), [47,0], 'empty map')
 
     t.deepEqual(
@@ -16,5 +16,10 @@ test('Serialize', t => {
     t.deepEqual(
         s.serialize(['int', 'bool', [[0,false], [1,true], [3,true]]]),
         [47,3,0,127,2,255,6,255]
+    )
+
+    t.deepEqual(
+        s.serialize(['int', 'bool', [[3,false], [5,true], [1,true]]]),
+        [47,3,2,255,6,127,10,255]
     )
 });
