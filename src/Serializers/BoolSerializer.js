93cb421ffa29ec020876370010cfef385e6acb4b
Dincho Todorov <dincho@users.noreply.github.com>
2020-03-11 15:55:23 +0200

    Add Bool deserializer

--- src/Serializers/BoolSerializer.js
+++ src/Serializers/BoolSerializer.js
@@ -1,10 +1,14 @@
 const FateTag = require('../FateTag.js')
+const FateBool = require('../types/FateBool.js')
 
 BoolSerializer = function () {}
 
 BoolSerializer.prototype = {
     serialize: function (data) {
         return (data.valueOf() === true) ? [FateTag.TRUE] : [FateTag.FALSE]
+    },
+    deserialize: function (data) {
+        return (data[0] === FateTag.TRUE) ? new FateBool(true) : new FateBool(false)
     }
 }
 
--- tests/Serializers/BoolSerializer.js
+++ tests/Serializers/BoolSerializer.js
@@ -16,3 +16,14 @@ test('Serialize', t => {
     t.deepEqual(s.serialize(new FateBool(0)), [127])
     t.deepEqual(s.serialize(new FateBool("")), [127])
 });
+
+test('Deserialize', t => {
+    const T = new FateBool(true)
+    const F = new FateBool(false)
+
+    t.deepEqual(s.deserialize([255]), T)
+    t.deepEqual(s.deserialize([127]), F)
+
+    t.deepEqual(s.deserialize(new Uint8Array([255])), T)
+    t.deepEqual(s.deserialize(new Uint8Array([127])), F)
+});
