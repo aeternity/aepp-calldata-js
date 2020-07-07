3fda876929a4af2ca2c3223fca8e3da0552967ee
Dincho Todorov <dincho@users.noreply.github.com>
2020-07-07 19:02:09 +0300

    Add ByteArray deserialization support

--- src/Serializers/ByteArraySerializer.js
+++ src/Serializers/ByteArraySerializer.js
@@ -1,12 +1,11 @@
 const FateTag = require('../FateTag.js')
+const FateByteArray = require('../types/FateByteArray.js')
 const IntSerializer = require('./IntSerializer.js')
 
 const intSerializer = new IntSerializer()
 
-ByteArraySerializer = function () {}
-
-ByteArraySerializer.prototype = {
-    serialize: function (data) {
+class ByteArraySerializer {
+    serialize(data) {
         if (data.length === 0) {
             return [FateTag.EMPTY_STRING]
         }
@@ -24,6 +23,40 @@ ByteArraySerializer.prototype = {
             FateTag.LONG_STRING,
             ...intSerializer.serialize(data.length - 64),
             ...data.valueOf()
+        ]
+    }
+    deserialize(data) {
+        const [value, rest] = this.deserializeStream(data)
+
+        return value
+    }
+    deserializeStream(data) {
+        data = new Uint8Array(data)
+        const prefix = data[0]
+
+        if (prefix === FateTag.EMPTY_STRING) {
+            return [
+                new FateByteArray(),
+                data.slice(1)
+            ]
+        }
+
+        if (prefix === FateTag.LONG_STRING) {
+            const [fateLen, rest] = intSerializer.deserializeStream(data.slice(1))
+            const len = Number(fateLen.valueOf()) + 64
+
+            return [
+                new FateByteArray(rest.slice(0, len)),
+                rest.slice(len)
+            ]
+        }
+
+        //short string
+        const offset = (prefix >> 2) + 1
+
+        return [
+            new FateByteArray(data.slice(1, offset)),
+            data.slice(offset)
         ]
     }
 }
--- tests/Serializers/ByteArraySerializer.js
+++ tests/Serializers/ByteArraySerializer.js
@@ -32,6 +32,7 @@ test('Serialize plain array', t => {
     )
 
     const longBytes = [...Array(64).keys()]
+    // console.log('long bytes', s.serialize(longBytes))
     t.deepEqual(
         s.serialize(longBytes),
         [1, 0].concat(longBytes),
@@ -49,6 +50,66 @@ test('Serialize plain array', t => {
     t.deepEqual(
         s.serialize(hugeBytes),
         [1, 111, 122].concat(hugeBytes),
+        'very long byte_array > 63'
+    )
+});
+
+test('Deserialize Stream', t => {
+    const longBytes = [...Array(64).keys()]
+
+    t.deepEqual(
+        s.deserializeStream([95, 5, 5, 5]),
+        [new FateByteArray([]), new Uint8Array([5, 5, 5])],
+        'empty byte_array'
+    )
+
+    t.deepEqual(
+        s.deserializeStream([17, 1, 2, 3, 4, 5, 5, 5]),
+        [new FateByteArray([1, 2, 3, 4]), new Uint8Array([5, 5, 5])],
+        'short byte_array'
+    )
+
+    t.deepEqual(
+        s.deserializeStream([
+           1,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9,
+          10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+          22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
+          34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45,
+          46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
+          58, 59, 60, 61, 62, 63, 5, 5, 5
+        ]),
+        [new FateByteArray(longBytes), new Uint8Array([5,5,5])],
+        'long byte_array'
+    )
+});
+
+test('Deserialize', t => {
+    t.deepEqual(s.deserialize([95]), new FateByteArray([]), 'empty byte_array')
+
+    t.deepEqual(
+        s.deserialize([17, 1, 2, 3, 4]),
+        new FateByteArray([1, 2, 3, 4]),
+        'short byte_array'
+    )
+
+    const longBytes = [...Array(64).keys()]
+    t.deepEqual(
+        s.deserialize([1, 0].concat(longBytes)),
+        new FateByteArray(longBytes),
+        'long byte_array > 64, short int (len - 64 < 64)'
+    )
+
+    const longerBytes = [...Array(128).keys()]
+    t.deepEqual(
+        s.deserialize([1, 111, 0].concat(longerBytes)),
+        new FateByteArray(longerBytes),
+        'long byte_array > 63, long int (len - 64 > 63)'
+    )
+
+    const hugeBytes = [...Array(250).keys()]
+    t.deepEqual(
+        s.deserialize([1, 111, 122].concat(hugeBytes)),
+        new FateByteArray(hugeBytes),
         'very long byte_array > 63'
     )
 });
