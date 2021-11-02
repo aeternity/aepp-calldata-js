944882c734d8cf4b69dcf398478a43cb25b29fa1
Denis Davidyuk <denis_davidyuk@hotmail.com>
2021-11-02 11:58:59 +0300

    Validate string in HexStringToByteArray

--- src/utils/HexStringToByteArray.js
+++ src/utils/HexStringToByteArray.js
@@ -1,10 +1,15 @@
+// TODO consider using Buffer.from(<string>, 'hex') instead
 const HexStringToByteArray = function (str) {
     const start = str.startsWith('0x') ? 2 : 0
-    let ints = [];
+    let ints = []
 
     for (let i = start, charsLength = str.length; i < charsLength; i += 2) {
-        const hex = str.substring(i, i + 2)
-        ints.push(parseInt(hex, 16));
+        const [int1, int2] = [0, 1].map(j => parseInt(str[i + j], 16))
+        const isOddEnd = i + 1 >= charsLength
+        if (Number.isNaN(int1) || Number.isNaN(int2) && !isOddEnd) {
+            throw new Error(`Invalid hex string: ${str}`)
+        }
+        ints.push(isOddEnd ? int1 : (int1 << 4) + int2)
     }
 
     return new Uint8Array(ints)
