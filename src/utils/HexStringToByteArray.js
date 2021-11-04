da9dc489fa039a3428f5961101e8d7200aae54f1
Denis Davidyuk <denis_davidyuk@hotmail.com>
2021-11-04 10:07:39 +0300

    Rewrite HexStringToByteArray using RegExp

--- src/utils/HexStringToByteArray.js
+++ src/utils/HexStringToByteArray.js
@@ -1,18 +1,13 @@
 // TODO consider using Buffer.from(<string>, 'hex') instead
 const HexStringToByteArray = function (str) {
-    const start = str.startsWith('0x') ? 2 : 0
-    let ints = []
-
-    for (let i = start, charsLength = str.length; i < charsLength; i += 2) {
-        const [int1, int2] = [0, 1].map(j => parseInt(str[i + j], 16))
-        const isOddEnd = i + 1 >= charsLength
-        if (Number.isNaN(int1) || Number.isNaN(int2) && !isOddEnd) {
-            throw new Error(`Invalid hex string: ${str}`)
-        }
-        ints.push(isOddEnd ? int1 : (int1 << 4) + int2)
+    const match = str.match(new RegExp(/^(0x)?([a-f0-9]*)$/, 'i'))
+    if (!match) {
+        throw new Error(`Invalid hex string: ${str}`)
     }
-
-    return new Uint8Array(ints)
+    return new Uint8Array(match[2]
+        .split(/(.{1,2})/)
+        .filter(el => el)
+        .map(el => parseInt(el, 16)))
 }
 
 module.exports = HexStringToByteArray
