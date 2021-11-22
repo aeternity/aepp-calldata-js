05f29f4fd0b83b290528e49d5cb47b5474241d82
Dincho Todorov <dincho@users.noreply.github.com>
2021-11-22 12:33:43 +0200

    Automate browser tests runner (#95)

--- tests/test.js
+++ tests/test.js
@@ -1,2 +1,5 @@
+// use ava test framework in nodejs environment and tape in browser
+// ava does not have in-browser runner unfortunately
+// see test-browser.js and package.json browser field
+
 module.exports = require('ava')
-// module.exports = require('tape')
