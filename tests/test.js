c86195386b43563a3d6fee9ec44195af06b676c0
Denis Davidyuk <denis_davidyuk@hotmail.com>
2022-02-09 14:58:16 +0300

    Update dependencies (#121)

--- tests/test.js
+++ tests/test.js
@@ -2,4 +2,6 @@
 // ava does not have in-browser runner unfortunately
 // see test-browser.js and package.json browser field
 
+// https://github.com/import-js/eslint-plugin-import/issues/2352
+// eslint-disable-next-line import/no-unresolved
 module.exports = require('ava')
