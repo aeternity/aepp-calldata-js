37453c3a07f74e7166a4160499f3c17bf94fa4c9
Denis Davidyuk <denis_davidyuk@hotmail.com>
2022-08-25 15:29:36 +0300

    Update aecli to 4.1.0 (#149)

--- tests/contract-call.sh
+++ tests/contract-call.sh
@@ -4,39 +4,36 @@ set -eu -o pipefail
 
 # Examples:
 # contract-call.sh test_empty
-# contract-call.sh test_bool "true" "false"
+# contract-call.sh test_bool "[true, false]"
 
 CONTRACT_SRC="contracts/Test.aes"
-ADDRESS_SRC="build/${CONTRACT_SRC//.aes/.addr}"
+DESCRIPTOR_SRC="build/${CONTRACT_SRC//.aes/.desc.json}"
 WALLET_PATH="build/wallet"
 WALLET_PASS="123"
 # init() calldata
-CALLDATA="cb_KxFE1kQfP4oEp9E="
+CALLDATA="[]"
 
-if ! aecli account address -P $WALLET_PASS $WALLET_PATH > /dev/null; then
-    ACCOUNT=$(aecli account create --json -P $WALLET_PASS $WALLET_PATH | jq -r '.publicKey')
+if ! npx aecli account address -P $WALLET_PASS $WALLET_PATH > /dev/null; then
+    ACCOUNT=$(npx aecli account create --json -P $WALLET_PASS $WALLET_PATH | jq -r '.publicKey')
     curl -sS -X POST https://faucet.aepps.com/account/$ACCOUNT > /dev/null
 fi
 
-if ! aecli account balance -P $WALLET_PASS $WALLET_PATH > /dev/null; then
+if ! npx aecli account balance -P $WALLET_PASS $WALLET_PATH > /dev/null; then
     echo "Please feed the wallet"
-    aecli account address -P $WALLET_PASS $WALLET_PATH
+    npx aecli account address -P $WALLET_PASS $WALLET_PATH
 fi
 
-if [ ! -f $ADDRESS_SRC ]; then
-    RESULT=$(aecli contract deploy --json -P $WALLET_PASS --networkId ae_uat $WALLET_PATH $CONTRACT_SRC $CALLDATA)
-    rm $(echo $RESULT | jq -r '.descPath')
-    echo $RESULT | jq -r '.address' > $ADDRESS_SRC
+if [ ! -f $DESCRIPTOR_SRC ]; then
+    npx aecli contract deploy \
+        --json -P $WALLET_PASS --descrPath $DESCRIPTOR_SRC --networkId ae_uat \
+        $WALLET_PATH --contractSource $CONTRACT_SRC $CALLDATA > /dev/null
 fi
-
-CONTRACT_ADDR=$(cat $ADDRESS_SRC)
 
 function contract_call() {
     echo -n "$1(${@:2}) -> "
-    aecli contract call \
+    npx aecli contract call \
         --networkId ae_uat --password $WALLET_PASS --json \
-        --contractAddress $CONTRACT_ADDR \
-        --contractSource $CONTRACT_SRC \
+        --descrPath $DESCRIPTOR_SRC \
         $WALLET_PATH \
         "$@" | jq -r '.result.returnValue'
 }
