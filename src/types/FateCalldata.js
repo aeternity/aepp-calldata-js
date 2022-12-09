1d0ab551fd8a0e8b9033ac139c1582efe5cdab13
Dincho Todorov <dincho@users.noreply.github.com>
2022-12-09 12:30:32 +0200

    Cleanup FateCalldata type

--- src/types/FateCalldata.js
+++ src/types/FateCalldata.js
@@ -1,6 +1,5 @@
 const FateTuple = require('./FateTuple')
 const FateByteArray = require('./FateByteArray')
-// const {FateTypeCalldata} = require('../FateTypes')
 const {symbolIdentifier} = require('../utils/hash')
 
 class FateCalldata extends FateTuple {
@@ -14,40 +13,6 @@ class FateCalldata extends FateTuple {
             [funcBytes, argsTuple]
         )
     }
-
-    // get valueTypes() {
-    //     return this._valueTypes
-    // }
-
-    // get type() {
-    //     return this._type
-    // }
-
-    // get size() {
-    //     return this._items.length
-    // }
-
-    // get items() {
-    //     return this._items
-    // }
-
-    // valueOf() {
-    //     return this.prepareItems(e => e.valueOf())
-    // }
-
-    // prepareItems(callback) {
-    //     const items = this._items.map(callback)
-
-    //     if (this._type.name === 'record') {
-    //         return zipObject(this._type.keys, items)
-    //     }
-
-    //     return items
-    // }
-
-    // accept(visitor) {
-    //     return visitor.visitTuple(this)
-    // }
 }
 
 module.exports = FateCalldata
