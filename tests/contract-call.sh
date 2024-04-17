d5c527137f9cae9ef041f0abdeae252fc34a3724
Denis Davidyuk <denis_davidyuk@hotmail.com>
2024-04-17 11:39:57 +1000

    Update aecli to 6.0.0

--- tests/contract-call.sh
+++ tests/contract-call.sh
@@ -12,30 +12,32 @@ WALLET_PATH="build/wallet"
 WALLET_PASS="123"
 # init() calldata
 CALLDATA="[]"
+export AECLI_NODE_URL="https://next.aeternity.io"
+export AECLI_COMPILER_URL="cli8"
 
-if ! npx aecli account address -P $WALLET_PASS $WALLET_PATH > /dev/null; then
+ACCOUNT=$(npx aecli account address --json $WALLET_PATH 2> /dev/null | jq -r '.publicKey' || true)
+if [ -z $ACCOUNT ]; then
     ACCOUNT=$(npx aecli account create --json -P $WALLET_PASS $WALLET_PATH | jq -r '.publicKey')
-    curl -sS -X POST https://faucet.aepps.com/account/$ACCOUNT > /dev/null
+    curl -sS -X POST https://faucet.ceres.aepps.com/account/$ACCOUNT > /dev/null
 fi
 
-if ! npx aecli account balance -P $WALLET_PASS $WALLET_PATH > /dev/null; then
-    echo "Please feed the wallet"
-    npx aecli account address -P $WALLET_PASS $WALLET_PATH
+if ! npx aecli inspect $ACCOUNT > /dev/null; then
+    echo "Please feed the wallet" $ACCOUNT
+    exit
 fi
 
 if [ ! -f $DESCRIPTOR_SRC ]; then
     npx aecli contract deploy \
-        --json -P $WALLET_PASS --descrPath $DESCRIPTOR_SRC --networkId ae_uat \
+        --json -P $WALLET_PASS --descrPath $DESCRIPTOR_SRC \
         $WALLET_PATH --contractSource $CONTRACT_SRC $CALLDATA > /dev/null
 fi
 
 function contract_call() {
     echo -n "$1(${@:2}) -> "
     npx aecli contract call \
-        --networkId ae_uat --password $WALLET_PASS --json \
+        --callStatic --json \
         --descrPath $DESCRIPTOR_SRC \
-        "$@" \
-        $WALLET_PATH | jq -r '.result.returnValue'
+        "$@" | jq -r '.result.returnValue'
 }
 
 contract_call "$@"
